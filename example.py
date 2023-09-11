203e2f0dd94a028dbc13fb396deb915f2d6855a1
yuvalNakav <yuvalnakav1@gmail.com>
2023-09-11 12:18:30 +0300

    relocation for helper funcs

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import ex11_utils as utils
 board = [
-    ["D", "C", "B", "A"],
+    ["D", "E", "QU", "U"],
 
     ["G", "A", "D", "E"],
 
@@ -12,9 +12,13 @@ words = []
 with open("boggle_dict.txt", "r") as txt_file:
     words = [word.split("\n")[0] for word in txt_file.readlines()]
 
+letters_3 = [word for word in words if len(word) == 3]
+# print(len(letters_3), letters_3)
 # print(len(words), words)
 # print("final",utils.is_valid_path(board, path, ["bed", "fit", "day", "did", "dad", "bad", "dcd"]))
-# paths = utils.find_length_n_paths(3, board, words)
-paths = utils.find_length_n_words(3, board, words)
+paths = utils.find_length_n_paths(3, board, words)
+paths2 = utils.find_length_n_words(3, board, words)
+# paths = utils.find_length_n_paths(5, board, words)
 # paths = utils.find_length_n_paths(5, board, [word.upper() for word in ["bed", "fit", "day"]])
 print("final", len(paths), paths)
+print("final2", len(paths2), paths2)
